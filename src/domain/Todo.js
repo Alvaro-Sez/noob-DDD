d315b05fd6e0cbe00ef99778e8132894fb649cd0
Alvaro-sez <arrovaalvaro@gmail.com>
2021-12-18 18:59:49 +0100

    trying

---
+++ src/domain/Todo.js
@@ -0,0 +1,10 @@
+class Todo {
+    constructor({id, task, createdAt, expiresAt}){
+        this.id = id;
+        this.task = task;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+    }
+
+    
+}
